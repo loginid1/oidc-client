8577a14cfb6fe997bcd8a83a7558d419648dd0e5
braagaa <michael_braga@outlook.com>
2021-07-02 17:22:23 -0400

    removed dotenv-webpack

--- assets/js/src/services.js
+++ assets/js/src/services.js
@@ -3,14 +3,14 @@ import { getEnvVariables } from "./utils";
 const { loginUrl, baseUrl, clientId } = getEnvVariables();
 
 export const requestToken = async (code, verifier) => {
-  const url = queryBuilder(loginUrl + "/token");
+  const url = loginUrl + "/oauth2/token";
   return await fetch(url, {
     method: "POST",
     body: JSON.parse({
       client_id: clientId,
       grant_type: "authorization_code",
       code,
-      redirect_uri: baseUrl,
+      redirect_uri: baseUrl + "/public",
       code_verifier: verifier,
     }),
   });
