b9bfaab561bb2eb3b38dccd7de5e330505237172
Raymond Neilson <rneilson@interloper.ca>
2021-07-05 17:50:28 -0300

    Exchange code for id token and display

--- routes/private.js
+++ routes/private.js
@@ -1,16 +1,48 @@
 "use strict;"
 
 const crypto = require("crypto");
+const fetch = require("node-fetch");
 const rs = require("jsrsasign");
 
 const clientId = process.env.PRIVATE_CLIENT_ID;
-const privKey = process.env.PRIVATE_CLIENT_KEY;
 const baseUrl = `${process.env.BASE_URL}/private`;
 const authUrl = `${process.env.LOGIN_URL}/oauth2/auth`;
 const tokenUrl = `${process.env.LOGIN_URL}/oauth2/token`;
 
-function makeJwt () {
+let privKey = null;
+if (process.env.PRIVATE_CLIENT_KEY) {
+    privKey = rs.KEYUTIL.getKey(process.env.PRIVATE_CLIENT_KEY);
+}
 
+function makeJwt () {
+    const header = {
+        "alg": "ES256",
+        "typ": "JWT"
+    };
+    const payload = {
+        "iss": clientId,
+        "sub": clientId,
+        "aud": tokenUrl,
+        "jti": crypto.randomBytes(12).toString('base64url'),
+        "exp": parseInt(new Date().valueOf() / 1000),
+    };
+    return rs.KJUR.jws.JWS.sign("ES256", JSON.stringify(header), JSON.stringify(payload), privKey);
+}
+
+async function parseJwt (jwt) {
+    if (!jwt) {
+        return {};
+    }
+
+    [headerStr, payloadStr, signature] = jwt.split(".");
+    const header = JSON.parse(Buffer.from(headerStr, "base64url").toString());
+    const payload = JSON.parse(Buffer.from(payloadStr, "base64url").toString());
+
+    if (header.kid) {
+        // TODO: verify sig
+    }
+
+    return { header, payload, signature };
 }
 
 async function privateClientHandler (req, res) {
@@ -20,19 +52,46 @@ async function privateClientHandler (req, res) {
 
     if (code) {
         // TODO: check state
-        // TODO: construct client assertion JWT
-        // TODO: call token endpoint
-        // TODO: parse id token
-        // TODO: check nonce
-        res.render("private_code", {
-            code,
-        });
-        return;
+
+        // Construct client assertion JWT and token endpoint params
+        const jwt = makeJwt();
+        const params = new URLSearchParams();
+        params.append("grant_type", "authorization_code");
+        params.append("code", code);
+        params.append("redirect_uri", baseUrl);
+        params.append("client_id", clientId);
+        params.append("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
+        params.append("client_assertion", jwt);
+        
+        // Call token endpoint
+        const response = await fetch(tokenUrl, { method: "POST", body: params });
+        const data = await response.json()
+
+        if (response.ok) {
+            // Parse id token
+            const idToken = data.id_token || "<none>";
+            const { payload } = await parseJwt(data.id_token);
+            const payloadStr = payload ? JSON.stringify(payload, undefined, 2) : undefined;
+
+            // TODO: check nonce
+
+            res.render("private_code", {
+                code,
+                idToken,
+                payloadStr,
+            });
+            return;
+        }
+
+        // Extract error from response
+        error = data.error || "unknown_error";
+        error_description = data.error_description || "An unknown error occured";
     }
 
     if (error) {
         // Convert error code to title
-        const title = error.replace("_", " ");
+        let title = error.replace("_", " ");
+        title = title[0].toUpperCase() + title.substr(1);
         res.render("private_error", { title, error, error_description });
         return;
     }
